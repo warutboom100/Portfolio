65ea9026e0aa24230fa5ce72745e6ddc644df6d7
Iboomnarak <warutboom100@gmail.com>
2025-03-17 16:51:04 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:process.env.VITE_BASE_PATH ||"/Portfolio",
   css:{
     modules:{
       localsConvention:"camelCase",
